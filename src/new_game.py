aaed318a7f754bc395e2822a3088059c090bc061
kozakvaleriy1505 <kozak.valeriy.1505@gmail.com>
2025-09-03 15:42:48 +0300

    add main and button(working)

---
+++ src/new_game.py
@@ -0,0 +1,16 @@
+import pygame
+
+def run(screen):
+    font = pygame.font.SysFont("arial", 48)
+    screen.fill((50, 80, 120))
+    text = font.render("Вікно створення нової гри", True, (255, 255, 255))
+    screen.blit(text, (200, 250))
+    pygame.display.flip()
+
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                waiting = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                waiting = False
